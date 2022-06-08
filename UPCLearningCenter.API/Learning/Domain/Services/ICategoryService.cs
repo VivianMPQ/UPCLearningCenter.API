211d3f44fcb1ad38f52db9ae6ac1b04c116f8b73
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-08 18:04:04 -0500

    feat(categories): implemented service interface

---
+++ UPCLearningCenter.API/Learning/Domain/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using UPCLearningCenter.API.Learning.Domain.Models;
+using UPCLearningCenter.API.Learning.Domain.Services.Comunication;
+
+namespace UPCLearningCenter.API.Learning.Domain.Services;
+
+public class ICategoryService
+{
+    Task<IEnumerable<Category>> ListAsync();
+    Task<CategoryResponse> SaveAsync(Category category);
+    Task<CategoryResponse> UpdateAsync(long id, Category category);
+    Task<CategoryResponse> DeleteAsync(long id);
+}
