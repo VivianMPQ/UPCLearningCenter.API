c2bd113f9cc948f213385bc4f7ce93f90476d5d9
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-08 18:26:38 -0500

    feat(categories) implement API endpoint controller

---
+++ UPCLearningCenter.API/Learning/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UPCLearningCenter.API.Learning.Domain.Services;
+
+namespace UPCLearningCenter.API.Learning.Controllers;
+
+//decoramos
+[ApiController]
+[Route("[controller]")]  //  ./Categories
+public class CategoriesController: ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+    
+
+//inyeccion de dependencia por consy¿tructor
+//generate -> constructor
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+    
+    
+    
+    
+}
