d6e40af399c7c610d37df3cf02616b01fc6c06c0
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-10 23:49:05 -0500

    feat(tutorials): added partial implement of inbound service

---
+++ UPCLearningCenter.API/Learning/Controllers/TutorialsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UPCLearningCenter.API.Learning.Domain.Models;
+using UPCLearningCenter.API.Learning.Domain.Services;
+using UPCLearningCenter.API.Learning.Resources;
+
+namespace UPCLearningCenter.API.Learning.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TutorialsController: ControllerBase
+{
+    private readonly ITutorialService _tutorialService;
+    private readonly IMapper _mapper;
+
+
+    public TutorialsController(ITutorialService tutorialService, IMapper mapper)
+    {
+        _tutorialService = tutorialService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<TutorialResource>> GetAllAsync()
+    {
+        var tutorials = await _tutorialService.ListAsync();
+        return _mapper.Map<IEnumerable<Tutorial>, IEnumerable<TutorialResource>>(tutorials);
+
+    }
+
+}
